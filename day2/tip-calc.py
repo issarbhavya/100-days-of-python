85007e73c5d4d057b22ed438d6e731495d620132
issarbhavya <ba1712@srmist.edu.in>
2022-05-20 03:29:08 +0530

    commit

---
+++ day2/tip-calc.py
@@ -0,0 +1,12 @@
+print ("welcome to the tip calculator")
+bill= float(input("what was the total bill? $"))
+tip_percent=int(input("what percentage of tip would you like to give 10 or 12 or 15? "))
+no_of_people=int(input("how many people to split the bill? "))
+
+tip=tip_percent/100*bill
+total_amount=bill+tip
+single_share=round(total_amount/no_of_people ,2)
+
+print (f"each person should pay: ${single_share}")
+
+
