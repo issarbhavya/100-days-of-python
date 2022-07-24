4bf0b2f7e246570de5d349b8691e1d11044ad96a
issarbhavya <ba1712@srmist.edu.in>
2022-07-24 13:59:05 +0530

    commit

---
+++ day4/rock_paper_sci2.py
@@ -0,0 +1,67 @@
+import random
+
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+choice="y"
+while(choice=="y"):
+    user_choice= int(input("enter 0 for rock, 1 for paper,2 for scissor "))
+            
+    computer_choice= random.randint(0,2)
+    for_print=[rock,paper,scissors]
+    print(for_print[user_choice])
+    print(for_print[computer_choice])
+
+    if (user_choice>2 or user_choice<0):
+        print ("invalid input")
+      
+    if user_choice==0:
+        if computer_choice==0:
+            print("tie")
+        elif computer_choice==1:
+            print("you losse")
+        elif computer_choice==2:
+            print("you win")
+
+    if user_choice==1:
+        if computer_choice==0:
+            print("you win")
+        elif computer_choice==1:
+            print("tie")
+        elif computer_choice==2:
+            print("you loose")     
+            
+    if user_choice==2:
+        if computer_choice==0:
+            print("you loose")
+        elif computer_choice==1:
+            print("you win")
+        elif computer_choice==2:
+            print("tie")
+    choice=input("enter y to cntinue playing :")        
+print ("\nThanks for playing")  
