4b5bea131add886f9b463b5e66cfa06853b65094
issarbhavya <103739096+issarbhavya@users.noreply.github.com>
2022-12-28 14:41:04 +0530

    c

--- day 18-25 (turtle projects)/day 18 (hirst painting and spirograph)/hirst painting/damien hirst painting.py
+++ day 18-25 (turtle projects)/day 18 (hirst painting and spirograph)/hirst painting/damien hirst painting.py
@@ -17,15 +17,15 @@ def draw_row(n):
     global l
     for i in range (0,n):
         colour=choose_color()
-        timmy.dot(10,colour)
-        timmy.fd(10)   
+        timmy.dot(20,colour)
+        timmy.fd(50)   
 
 timmy.setpos(-200,-200)
 y=timmy.ycor()
 timmy.shape("turtle")
 for i in range(0,10):
-    draw_row(30)
-    y=y+10
+    draw_row(10)
+    y=y+50
     timmy.setpos(-200,y)
 
 
